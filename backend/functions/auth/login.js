c246bc8ffc0b64343a4ee9cd01d45f9623c6f42a
SalmanAzamDev <salmankhach@gmail.com>
2025-08-11 15:50:46 +0500

    Added login APIs

---
+++ backend/functions/auth/login.js
@@ -0,0 +1,26 @@
+const bcrypt = require('bcrypt');
+const pool = require('../database/database');
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+        if (user.rows.length === 0) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        const validPassword = await bcrypt.compare(password, user.rows[0].password);
+        if (!validPassword) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        res.status(200).json({ message: 'Login successful',
+        userId: user.rows[0].user_id });
+
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+module.exports = login;
