30c768992bea6eb63a3139eb7391212e18b81434
SalmanAzamDev <salmankhach@gmail.com>
2025-08-11 17:17:13 +0500

    Added splashscreen index.tsx

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,15 +1,38 @@
-import { Text, View } from "react-native";
+import { View, Text, Dimensions, Image } from "react-native";
+import { useEffect } from "react";
+import { router } from "expo-router";
+import LottieView from "lottie-react-native";
 
-export default function Index() {
+const { height } = Dimensions.get("window");
+
+export default function SplashScreen() {
+  useEffect(() => {
+    setTimeout(() => {
+      router.replace("/onboarding/welcome");
+    }, 2500);
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ flex: 1, backgroundColor: "#ffffff", justifyContent: "center", alignItems: "center" }}>
+
+      <Image
+        source={require("@/assets/images/logo.png")}
+        style={{ width: 130, height: 130, marginBottom: 20, }}
+      /> 
+
+      <Text style={{ fontSize: 28, fontWeight: "600", color: "#000000" }}>AchieveIt</Text>
+
+      <LottieView
+        source={require("@/assets/lottie/loader.json")}
+        autoPlay
+        loop
+        style={{
+          position: "absolute",
+          bottom: height * 0.09,
+          width: 90,
+          height: 90,
+        }}
+      />
     </View>
   );
 }
