ff9006f8bc816e347223a0c47ba41a4489e7f900
SalmanAzamDev <salmankhach@gmail.com>
2025-08-12 13:05:23 +0500

    Updated schema

--- backend/database-schema.sql
+++ backend/database-schema.sql
@@ -12,8 +12,12 @@ CREATE TABLE users (
 CREATE TABLE IF NOT EXISTS tasks (
     task_id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
-    task_name VARCHAR(150) NOT NULL,
+    title VARCHAR(150) NOT NULL,
+    description TEXT,
+    due_date DATE,
+    due_time TIME,
     priority INT NOT NULL CHECK (priority >= 1 AND priority <= 10),
+    completed BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
