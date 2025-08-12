af2d847f1bc33be74cfa4ec8df680bc893894d37
SalmanAzamDev <salmankhach@gmail.com>
2025-08-12 12:48:02 +0500

    Updated signup API logic

--- backend/functions/auth/signup.js
+++ backend/functions/auth/signup.js
@@ -2,25 +2,27 @@ const bcrypt = require('bcrypt');
 const pool = require('../../database/database');
 
 const signup = async (req, res) => {
-    const { username, email, password } = req.body;
+    const { username, password } = req.body;
 
     try {
-        const hashedPassword = await bcrypt.hash(password, 10)
-        const user = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+        const hashedPassword = await bcrypt.hash(password, 10);
 
+        const user = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
         if (user.rows.length > 0) {
             return res.status(400).json({ message: 'User already exists' });
         }
 
-        await pool.query('INSERT INTO users (username, email, password) VALUES ($1, $2, $3)', [username, email, hashedPassword]);
+        // Insert new user
+        await pool.query(
+            'INSERT INTO users (username, password) VALUES ($1, $2)',
+            [username, hashedPassword]
+        );
 
         res.status(201).json({ message: 'User created successfully' });
-
     } catch (error) {
-
+        console.error(error);
         res.status(500).json({ message: 'Internal server error' });
     }
-
 };
 
 module.exports = signup;
