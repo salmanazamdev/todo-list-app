a88a99bfb4d00be6fbdd3ede7520d2db2cf68bb1
SalmanAzamDev <salmankhach@gmail.com>
2025-08-13 00:37:33 +0500

    Updated naming

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,23 +1,13 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
-
-//Below are imports done for tabs' icons
 import Octicons from '@expo/vector-icons/Octicons';
 import Ionicons from '@expo/vector-icons/Ionicons';
-import Feather from '@expo/vector-icons/Feather';
-import AntDesign from '@expo/vector-icons/AntDesign';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-
-
 import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
 
 export default function TabLayout() {
-
   return (
     <Tabs
       screenOptions={{
@@ -26,54 +16,38 @@ export default function TabLayout() {
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
+          ios: { position: 'absolute' },
           default: {},
         }),
       }}>
-
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: 'Index',
           tabBarIcon: ({ color }) => <Octicons name="home" size={24} color="lightgray" />,
         }}
       />
-        <Tabs.Screen
-        name="orders"
+      <Tabs.Screen
+        name="calendar"
         options={{
-          title: 'Orders',
-          tabBarIcon: ({ color }) => <MaterialIcons name="shopping-cart-checkout" size={24} color="lightgray" />,
+          title: 'Calendar',
+          tabBarIcon: ({ color }) => <Ionicons name="calendar-outline" size={24} color="lightgray" />,
         }}
       />
-
-        <Tabs.Screen
-        name="message"
+      <Tabs.Screen
+        name="focus"
         options={{
-          title: 'Message',
-          tabBarIcon: ({ color }) => <AntDesign name="message1" size={24} color="lightgray" />,
+          title: 'Focus',
+          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="target" size={24} color="lightgray" />,
         }}
       />
-
-        <Tabs.Screen
-        name="ewallet"
-        options={{
-          title: 'E-Wallet',
-          tabBarIcon: ({ color }) => <Ionicons name="wallet-outline" size={24} color="lightgray" />,
-        }}
-      />
-
-        <Tabs.Screen
+      <Tabs.Screen
         name="profile"
         options={{
           title: 'Profile',
           tabBarIcon: ({ color }) => <MaterialCommunityIcons name="head-outline" size={24} color="lightgray" />,
         }}
       />
-  
-    
     </Tabs>
   );
 }
