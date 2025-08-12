4ca1b5309a55d95cac248d0dbba3d95bc6fcd479
SalmanAzamDev <salmankhach@gmail.com>
2025-08-12 12:49:36 +0500

    Updated login API logic

--- backend/functions/auth/login.js
+++ backend/functions/auth/login.js
@@ -2,10 +2,10 @@ const bcrypt = require('bcrypt');
 const pool = require('../../database/database');
 
 const login = async (req, res) => {
-    const { email, password } = req.body;
+    const { username, password } = req.body;
 
     try {
-        const user = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+        const user = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
         if (user.rows.length === 0) {
             return res.status(401).json({ message: 'Invalid credentials' });
         }
