227ea663e50976359c6c147911b41713d048a851
SalmanAzamDev <salmankhach@gmail.com>
2025-08-13 11:10:21 +0500

    Added Catergories API handlers and routes in index.js

--- backend/index.js
+++ backend/index.js
@@ -20,17 +20,24 @@ app.post('/signup', loggin, signup);
 app.post('/login', loggin, login);
 
 
-// Tasks handlers
+// Tasks handlers and routes
 const addTask = require('./functions/tasks/addTask');
 const getTasksByUserId = require('./functions/tasks/getTasksByUserId');
 const updateTask = require('./functions/tasks/updateTask');
 const deleteTask = require('./functions/tasks/deleteTask');
 
-// Tasks Routes
 app.post('/tasks', loggin, addTask);
 app.get('/tasks/:userId', loggin, getTasksByUserId);
 app.put('/tasks/:taskId', loggin, updateTask);
 app.delete('/tasks/:taskId', loggin, deleteTask);
+
+
+// Categories handlers and routes
+const addCategory = require('./functions/categories/addCategory');
+const getCategories = require('./functions/categories/getCategories');
+
+app.post('/categories', loggin, addCategory);
+app.get('/categories/:userId', loggin, getCategories);
 
 // Start the Server
 app.listen(PORT, () => {
