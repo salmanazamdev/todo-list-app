e1c3c8f957b9ea87c58cd57f60fa4ef3b5221cdc
SalmanAzamDev <salmankhach@gmail.com>
2025-08-14 17:19:41 +0500

    Updated getCategories API logic

--- backend/functions/categories/getCategories.js
+++ backend/functions/categories/getCategories.js
@@ -1,11 +1,9 @@
 const pool = require('../../database/database');
 
 const getCategories = async (req, res) => {
-  const { userId } = req.params;
   try {
     const result = await pool.query(
-      `SELECT * FROM categories WHERE user_id = $1`,
-      [userId]
+      `SELECT * FROM categories`
     );
     res.status(200).json({ categories: result.rows });
   } catch (err) {
