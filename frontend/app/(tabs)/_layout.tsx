5f9826fbab9964ebd0ecb40a3ac258022b143d2d
SalmanAzamDev <salmankhach@gmail.com>
2025-08-16 20:23:23 +0500

    Updated layout and tab bar

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -11,50 +11,45 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: '#8A4DFF',
-        tabBarInactiveTintColor: '#bbb',
+        tabBarActiveTintColor: '#8875FF',
+        tabBarInactiveTintColor: 'white',
         tabBarStyle: {
-          backgroundColor: '#111',
-          borderColor: "black",
+          backgroundColor: '#181818',
+          borderTopColor: 'transparent',
+          borderTopWidth: 0,
         },
-        
         tabBarLabelStyle: { fontSize: 13, marginBottom: 8 },
-        tabBarItemStyle: { alignItems: 'center', justifyContent: 'center', },
-
+        tabBarItemStyle: { alignItems: 'center', justifyContent: 'center' },
         headerShown: false,
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: { position: 'absolute' },
-          default: {},
-        }),
       }}>
       <Tabs.Screen
         name="index"
         options={{
           title: 'Index',
-          tabBarIcon: ({ color }) => <Octicons name="home" size={24} color="lightgray" />,
+          tabBarIcon: ({ color }) => <Octicons name="home" size={24} color={color} />,
         }}
       />
       <Tabs.Screen
         name="calendar"
         options={{
           title: 'Calendar',
-          tabBarIcon: ({ color }) => <Ionicons name="calendar-outline" size={24} color="lightgray" />,
+          tabBarIcon: ({ color }) => <Ionicons name="calendar-outline" size={24} color={color} />,
         }}
       />
       <Tabs.Screen
         name="focus"
         options={{
           title: 'Focus',
-          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="target" size={24} color="lightgray" />,
+          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="target" size={24} color={color} />,
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
           title: 'Profile',
-          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="head-outline" size={24} color="lightgray" />,
+          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="head-outline" size={24} color={color} />,
         }}
       />
     </Tabs>
