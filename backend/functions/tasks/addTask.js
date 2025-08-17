82e556e1777828cb5f506266d68a7c71fdc19467
SalmanAzamDev <salmankhach@gmail.com>
2025-08-17 15:24:57 +0500

    Updated addtask api query

--- backend/functions/tasks/addTask.js
+++ backend/functions/tasks/addTask.js
@@ -1,16 +1,16 @@
 const pool = require('../../database/database');
 
 const addTask = async (req, res) => {
-  const { userId, title, description, dueDate, dueTime, priority } = req.body;
+const { userId, title, description, dueDate, dueTime, priority, category_id } = req.body;
 
   try {
     const result = await pool.query(
-      `INSERT INTO tasks 
-        (user_id, title, description, due_date, due_time, priority) 
-       VALUES ($1, $2, $3, $4, $5, $6) 
-       RETURNING *`,
-      [userId, title, description, dueDate, dueTime, priority]
-    );
+  `INSERT INTO tasks 
+    (user_id, title, description, due_date, due_time, priority, category_id) 
+   VALUES ($1, $2, $3, $4, $5, $6, $7) 
+   RETURNING *`,
+  [userId, title, description, dueDate, dueTime, priority, category_id]
+);
 
     res.status(201).json(result.rows[0]);
   } catch (err) {
